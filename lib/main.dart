776a6449e78523cab5f370d223562441ee6be6f4
Guru Kalyan Paul <guru713146@gmail.com>
2026-02-10 17:11:07 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'features/live/live_page.dart';
-import 'features/cluster/cluster_page.dart';
-import 'features/detail/detail_page.dart';
+
 
 void main() {
   runApp(const ProviderScope(child: App()));
